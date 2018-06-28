14e305cd903986898fd4dc470201b0ea039dbddb
alebar <bardotalexandre@gmail.com>
2018-06-28 17:07:02 +0200

    ceci est l'exercice de mercredi avec le bouton js

---
+++ js/index.js
@@ -0,0 +1,43 @@
+function mypopup(){
+
+		alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}
+function callpopup() {
+
+		setTimeout(mypopup, 10000);
+}
+function psychedelique() {
+
+document.getElementById("body").style.backgroundColor = "magenta";
+
+document.getElementById("header").style.fontFamily = "Comic Sans MS";
+
+var Para = document.getElementsByTagName("p");
+    for (var i = 0; i < Para.length; i++) { 
+        Para[i].style.color = "blue";
+        Para[i].style.fontFamily = "papyrus";
+    
+}
+    var Para = document.getElementsByTagName("h1");
+    for (var i = 0; i < Para.length; i++) { 
+        Para[i].style.color = "green";
+        
+ }
+
+
+   var Para = document.getElementsByTagName("h2");
+    for (var i = 0; i < Para.length; i++) { 
+        Para[i].style.color = "green";
+        
+ }
+
+
+
+   var Para = document.getElementsByTagName("h3");
+    for (var i = 0; i < Para.length; i++) { 
+        Para[i].style.color = "green";
+        
+ }
+
+
+}
